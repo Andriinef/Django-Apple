f9075f789565b5e11e8b79eac63bd27bb5c9093e
Andriinef <andrii.nefedov@nure.ua>
2022-07-27 15:45:06 +0300

    lesson 9

--- coolsite/apple/urls.py
+++ coolsite/apple/urls.py
@@ -5,5 +5,11 @@ from .views import *
 urlpatterns = [
     path('', apple, name="apple"),
     path('ipad/', ipad, name="ipad"),
-    path('iphone/', iphone, name="about"),
+    path('iphone/', iphone, name="iphone"),
+    path('imac/', imac, name="imac"),
+    path('add_page/', add_page, name="add_page"),
+    path('contact/', contact, name="contact"),
+    path('login/', login, name="login"),
+    path('post/<int:post_id>/', show_post, name="post"),
+    path('category/<int:cat_id>/', show_category, name="category"),
 ]
--- coolsite/apple/views.py
+++ coolsite/apple/views.py
@@ -3,38 +3,78 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu = ["Продукция APPLE", "Добавить статью", "Обратная связь", "Войти"]
+menu = [{'title': "Продукция APPLE", 'url_name': "apple"},
+        {'title': "Добавить статью", 'url_name': "add_page"},
+        {'title': "Обратная связь", 'url_name': "contact"},
+        {'title': "Войти", 'url_name': "login"},
+        ]
 
 
 # Create your views here.
 def apple(request):
-    return render(request, "apple/apple.html", {"menu": menu, "title": "Продукция APPLE"})
+    posts = Apple.objects.filter(pk__lte=10)
+    cats = Category.objects.all()
+
+    context = {"menu": menu,
+               "posts": posts,
+               "cats": cats,
+               "title": "Продукция APPLE",
+               "cat_selected": 0,
+               }
+    return render(request, "apple/apple.html", context=context)
 
 
 def ipad(request):
-    posts = Apple.objects.filter(pk__lte=5)
-    return render(request, "apple/ipad.html", {"menu": menu, "posts": posts, "title": "iPad APPLE"})
+    context = {"menu": menu,
+               "title": "iPad APPLE"
+               }
+    return render(request, "apple/ipad.html", context=context)
 
 
 def iphone(request):
-    return render(request, "apple/iphone.html", {"menu": menu, "title": "iPhone APPLE"})
+    context = {"menu": menu,
+               "title": "iPhone APPLE"
+               }
+    return render(request, "apple/iphone.html", context=context)
 
 
-def categories(request, cat_slug):
-    if request.GET:
-        print(request.GET)
-    elif request.POST:
-        print(request.POST)
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{cat_slug}</p>")
+def imac(request):
+    context = {"menu": menu,
+               "title": "iMac APPLE"
+               }
+    return render(request, "apple/imac.html", context=context)
 
 
-def archive(request, year):
-    if int(year) < 999:
-        raise Http404()
-    elif int(year) > 2022:
-        return redirect('home', permanent=True)
+def add_page(request):
+    return HttpResponse("Добавить статью")
 
-    return HttpResponse(f"<h1>Архивы по годам</h1><p>{year}</p>")
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Автоматизация")
+
+
+def show_post(request, post_id):
+    return HttpResponse(f"Отображение статьи с id = {post_id}")
+
+
+def show_category(request, cat_id):
+    posts = Apple.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404
+
+    context = {"menu": menu,
+               "posts": posts,
+               "cats": cats,
+               "title": "Категории APPLE",
+               "cat_selected": cat_id,
+               }
+    return render(request, "apple/apple.html", context=context)
 
 
 def pageNotFound(request, exception):
