6a7d77cc37eb7f0a3096a7773b465b00f1bf0952
Andriinef <andrii.nefedov@nure.ua>
2022-08-05 13:42:54 +0300

    lesson 26

--- coolsite/apple/views.py
+++ coolsite/apple/views.py
@@ -25,17 +25,6 @@ class AppleHome(DataMixin, ListView):
         return Apple.objects.filter(is_published=True).select_related('cat')
 
 
-# def apple(request):
-#     posts = Apple.objects.all
-#
-#     context = {"menu": menu,
-#                "posts": posts,
-#                "title": "Продукцiя APPLE",
-#                "cat_selected": 0,
-#                }
-#     return render(request, "apple/apple.html", context=context)
-
-
 def ipad(request):
     context = {"menu": menu,
                "title": "iPad APPLE"
@@ -69,21 +58,6 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         return dict(list(context.items()) + list(c_def.items()))
 
 
-# def add_page(request):
-#     if request.method == 'POST':
-#         form = Add_Post_Fofm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # print(form.cleaned_data)
-#             form.save()
-#             return redirect('apple')
-#     else:
-#         form = Add_Post_Fofm()
-#     return render(request, "apple/add_page.html", {'form': form, 'menu': menu, 'title': "Додати статтю"})
-
-
-# def contact(request):
-#     return HttpResponse("Зворотній зв'язок")
-
 class ContactFormView(DataMixin, FormView):
     form_class = ContactForm
     template_name = 'apple/contact.html'
@@ -99,10 +73,6 @@ class ContactFormView(DataMixin, FormView):
         return redirect('apple')
 
 
-# def login(request):
-#     return HttpResponse("Автоматизація")
-
-
 class ShowPost(DataMixin, DetailView):
     model = Apple
     template_name = "apple/post.html"
@@ -113,19 +83,6 @@ class ShowPost(DataMixin, DetailView):
         context = super().get_context_data(**kwargs)
         c_def = self.get_user_context(title=context["post"])
         return dict(list(context.items()) + list(c_def.items()))
-
-
-# def show_post(request, post_slug):
-#     post = get_object_or_404(Apple, slug=post_slug)
-#
-#     context = {
-#         "post": post,
-#         "menu": menu,
-#         "title": post.title,
-#         "cat_selected": post.cat_id,
-#     }
-#
-#     return render(request, 'apple/post.html', context=context)
 
 
 class AppleCategory(DataMixin, ListView):
@@ -143,20 +100,6 @@ class AppleCategory(DataMixin, ListView):
         c_def = self.get_user_context(title="Категорія - " + str(c.name),
                                       cat_selected=c.pk)
         return dict(list(context.items()) + list(c_def.items()))
-
-
-# def show_category(request, cat_id):
-#     posts = Apple.objects.filter(cat_id=cat_id)
-#
-#     if len(posts) == 0:
-#         raise Http404
-#
-#     context = {"menu": menu,
-#                "posts": posts,
-#                "title": "Категорії APPLE",
-#                "cat_selected": cat_id,
-#                }
-#     return render(request, "apple/apple.html", context=context)
 
 
 def pageNotFound(request, exception):
