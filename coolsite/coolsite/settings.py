ba1f27328fcba675b36053bd256b53032ebf4844
Andriinef <andrii.nefedov@nure.ua>
2022-08-05 21:48:57 +0300

    Heroku_migrate

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rvdt+%+#h84c@&53nf673u8qw2sow-$84#cc6s)i&thjtpbxdp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'ddah.herokuapp.com']
 
