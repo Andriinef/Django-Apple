5ec85d1439915080217a0a1590e7a8c49e3462d6
Andriinef <andrii.nefedov@nure.ua>
2022-08-15 19:46:57 +0300

    ex_1

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -84,11 +84,16 @@ DATABASES = {
     # }
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": os.environ["DB_HOST"],
-        "NAME": os.environ["DB_NAME"],
-        "USER": os.environ["DB_USER"],
-        "PASSWORD": os.environ["DB_PASSWORD"],
-        "PORT": os.environ["DB_PORT"],
+        # "HOST": os.environ["DB_HOST"],
+        # "NAME": os.environ["DB_NAME"],
+        # "USER": os.environ["DB_USER"],
+        # "PASSWORD": os.environ["DB_PASSWORD"],
+        # "PORT": os.environ["DB_PORT"],
+        "HOST": "localhost",
+        "PORT": "5432",
+        "NAME": "test_db_an",
+        "USER": "andrii",
+        "PASSWORD": "5048",
     }
 }
 
