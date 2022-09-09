233df3ddfc31fbb4962df7a073777b0da7761563
Andriinef <andrii.nefedov@nure.ua>
2022-09-09 18:08:22 +0300

    MEDIA_ROOT

--- coolsite/settings.py
+++ coolsite/settings.py
@@ -163,7 +163,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_FILES_DIRS = [os.path.join(BASE_DIR, 'apple/static')]
 
-MEDIA_ROOT = ''
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 MEDIA_URL = '/media/'
 
 
