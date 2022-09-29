2ec46590add452cbafab04a9f5df09d3e747542f
Andriinef <andrii.nefedov@nure.ua>
2022-09-29 16:17:56 +0300

    urls

--- coolsite/urls.py
+++ coolsite/urls.py
@@ -25,15 +25,13 @@ urlpatterns = [
     path('', include('apple.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
-
     urlpatterns = [
-                      path('__debug__/', include(debug_toolbar.urls))
-                  ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-handler404 = pageNotFound
+        path('__debug__/', include(debug_toolbar.urls)),  # Debug Toolbar
+    ] + urlpatterns
